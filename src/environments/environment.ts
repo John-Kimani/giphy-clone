28532a93a57fe168a1419e4ccd1bd5ac64644e7d
John-Kimani <kimwaweru100@gmail.com>
2022-02-11 17:35:52 +0300

    creates data service and saves generated giphy api key placed in environments file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  giphyApiKey: 'YHnb3KzflJmWL7RGDcujepjR1dz8EFmG'
+  
 };
 
 /*
