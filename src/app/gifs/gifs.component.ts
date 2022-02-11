1cc6fd46bf6bca73119f8aa3bbe59a18200f4074
John-Kimani <kimwaweru100@gmail.com>
2022-02-11 17:40:02 +0300

    imports data service on gifs component

---
+++ src/app/gifs/gifs.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-gifs',
+  templateUrl: './gifs.component.html',
+  styleUrls: ['./gifs.component.css']
+})
+export class GifsComponent implements OnInit {
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+  }
+
+}
